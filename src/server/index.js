135746c09f101b7c2b2715002bc9254251e2ebe2
IanRock542 <i32505@gmail.com>
2024-03-04 14:23:59 -0600

    created server folder and moved all files into it

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,11 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import clientRoutes from ".routes/client.js"
+import generalRoutes from ".routes/general.js"
+import managementRoutes from ".routes/management.js"
+import salesRoutes from ".routes/sales.js"
+
 
 /* CONFIG */
 dotenv.config();
