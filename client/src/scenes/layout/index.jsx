bdc1903458eeb4bc20c6f3fe955f5b20acf61689
tatianacue <122386464+tatianacue@users.noreply.github.com>
2024-04-02 14:47:50 -0400

    Update sidebar user

--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -16,6 +16,7 @@ const Layout = () => {
   return (
     <Box display={isNonMobile ? "flex": "block"} width ="100%" height="100%">
       <Sidebar
+        user={data || {}}
         isNonMobile={isNonMobile}
         drawerWidth="250px"
         isSidebarOpen={isSidebarOpen}
@@ -23,6 +24,7 @@ const Layout = () => {
       />
       <Box>
         <Navbar 
+          //user={data || {}}
           isSidebarOpen={isSidebarOpen}
           setIsSidebarOpen={setIsSidebarOpen}
         />
