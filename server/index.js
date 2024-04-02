caf68162cac88877ac7cde904dcb00eba633f914
tatianacue <122386464+tatianacue@users.noreply.github.com>
2024-04-02 13:55:17 -0400

    Import data
    imported data to our MongoDB database

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ import generalRoutes from "./routes/general.js"
 import managementRoutes from "./routes/management.js"
 import salesRoutes from "./routes/sales.js"
 
+// data imports
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
 
 /* CONFIG */
 dotenv.config();
@@ -36,4 +39,7 @@ mongoose
     useUnifiedTopology: true
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+
+    /* ONLY ADD DATA ONE TIME */
+    User.insertMany(dataUser);
 }).catch((error ) => console.log(`${error} did not connect`))
