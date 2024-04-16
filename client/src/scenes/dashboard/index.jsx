c0f359a51f2f64e3a353910ac6e9d6ebad94d3d1
Jacob <jcm18e@fsu.edu>
2024-04-16 12:36:59 -0400

    Fixed some bugs and updated a color
    Fixed some bugs found as well as updated the coloring for a scroll bar

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
-import FlexBetween from 'components/FlexBetween';
-import Header from 'components/Header';
-import { DownloadOutlined, Email, PointOfSale, PersonAdd, Traffic,} from "@mui/icons-material";
-import { Box, Button, Typography, useTheme, useMediaQuery,} from "@mui/material";
+import React from "react";
+import FlexBetween from "components/FlexBetween";
+import Header from "components/Header";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import BreakdownChart from "components/BreakdownChart";
 import OverviewChart from "components/OverviewChart";
