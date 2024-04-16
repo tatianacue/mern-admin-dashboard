dcd58a9c2479cd673975a88ea40763b238248578
rb561 <134433728+rb561@users.noreply.github.com>
2024-04-16 11:10:24 -0400

    Dashboard changes
    Finished Dashboard page

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
     reducerPath: "adminApi",
-    tagTypes: ["User", "Products", "Customers", "Transactions", "Geography", "Sales"],
+    tagTypes: ["User", "Products", "Customers", "Transactions", "Geography", "Sales", "Dashboard"],
     endpoints: (build) => ({
         getUser: build.query({
             query: (id) => `general/user/${id}`,
@@ -33,6 +33,10 @@ export const api = createApi({
         query: () => "sales/sales",
         providesTags: ["Sales"],
         }),
+        getDashboard: build.query({
+            query: () => "general/dashboard",
+            providesTags: ["Dashboard"]
+        })
     }),
 });
 
@@ -43,4 +47,5 @@ export const {
     useGetTransactionsQuery,
     useGetGeographyQuery,
     useGetSalesQuery,
+    useGetDashboardQuery,
 } = api;
